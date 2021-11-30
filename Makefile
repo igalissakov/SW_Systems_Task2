db526f66b0a5b48a896aba015eb5519ce7eab48c
igalissakov <93942479+igalissakov@users.noreply.github.com>
2021-11-30 17:35:41 +0200

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=gcc
-AR=ar 
+AR=ar
 FLAGS= -Wall -g
 .PHONY: all
 
@@ -8,11 +8,20 @@ all: lib.a main.o
 
 connections: lib.a main.o
 	$(CC) $(FLAGS) -o main.o my_mat.o connections
+
 lib.a: my_mat.o
 	$(AR) rcs -o lib.a my_mat.o
+
 my_mat.o: my_mat.c my_mat.h
-	$(CC) $(FLAGS) -c my_mat.c
-main.o: my_mat.h main.c
+	$(CC) $(FLAGS) -c my_mat.c 
+
+main.o: main.c my_mat.h
 	$(CC) $(FLAGS) -c main.c
+
+run:
+	gcc my_mat.c
+	./a.out
+
+
 clean:
 	rm -f *.o *.a *.so *.out
--- my_mat.c
+++ my_mat.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #define n 10
-#define inf 999999999
+#define inf 99999999
 int min(int a, int b)
 {
-    if (a > b)
+    if (a < b)
         return a;
     else
         return b;
@@ -14,18 +14,38 @@ void getMatrix(int mat[n][n])
     {
         for (int j = 0; j < n; j++)
         {
-            scanf("%d", &mat[i][j]);
+            scanf(" %d",&mat[i][j]);
+            if(mat[i][j]==0 && i!=j)
+            {
+                mat[i][j]=inf;
+            }
+        }
+    }
+}
+void Floyd_Warshall(int mat[n][n], int k)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                mat[i][j] = 0;
+            }
+            else if (i == k || j == k){
+                continue;
+            }
+            else
+            {
+                mat[i][j] = min((mat[i][k] + mat[k][j]), mat[i][j]);
+            }
         }
     }
 }
 void isRoute(int mat[n][n])
 {
     int i, j;
-    scanf("%d%d", &i, &j);
-    if (mat[i][j] == 0 && i != j)
-    {
-        mat[i][j] = inf;
-    }
+    scanf(" %d %d",&i,&j);
     for (int i = 0; i < n; i++)
     {
         Floyd_Warshall(mat, i);
@@ -35,27 +55,11 @@ void isRoute(int mat[n][n])
     else
         printf("True\n");
 }
-void Floyd_Warshall(int mat[n][n], int k)
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (i == j)
-                mat[i][j] = 0;
-            else if (i == k || j == k)
-                continue;
-            else
-            {
-                mat[i][j] = min(mat[i][k] + mat[k][j], mat[i][j]);
-            }
-        }
-    }
-}
+
 void printShortPath(int mat[n][n])
 {
     int i, j;
-    scanf("%d%d", &i, &j);
+    scanf(" %d %d", &i, &j);
     for (int i = 0; i < n; i++)
     {
         Floyd_Warshall(mat, i);
@@ -65,3 +69,5 @@ void printShortPath(int mat[n][n])
     else
         printf("-1\n");
 }
+
+
